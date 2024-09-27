5b22bea7f8b711b375177bcdb6517eedf643352a
PiffTheeMonoRepoEnjoyer <gage.n.way@gmail.com>
2024-09-27 16:10:17 -0400

    let's see if this does it

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker build --no-cache -t cia_bot_public:latest . && \
 docker image prune -f && \
-docker run --detach --name cia-bot --network="host" --rm cia_bot_public:latest && \
-docker logs cia-bot -f
+docker run --detach --name cia-bot-public --network="host" --rm cia_bot_public:latest && \
+docker logs cia-bot-public -f
